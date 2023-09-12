8d2dd761809bff6d61b35d14b329a25b0c53ae6e
C0012S <alwaysh1002@gmail.com>
2023-09-12 18:22:47 +0900

    [COMMENT] TeddyBearFactory

--- Park-Sanghee/FactoryMethod/src/Doll/TeddyBearFactory.java
+++ Park-Sanghee/FactoryMethod/src/Doll/TeddyBearFactory.java
@@ -3,6 +3,11 @@ package Doll;
 import Factory.Doll;
 import Factory.Factory;
 
+/**
+ * ConcreteCreator : 곰인형을 만들고 꾸미는 공장 클래스
+ * @version 1.0
+ * @author Park SangHee
+ **/
 public class TeddyBearFactory extends Factory {
 	@Override
 	protected Doll createDoll(String animal, String dollName) {
