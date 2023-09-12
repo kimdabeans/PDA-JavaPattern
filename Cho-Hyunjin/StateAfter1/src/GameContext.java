e295fe1be13a91a798e29353fef1ecd485f3f534
Hyunjin02 <70645020+Hyunjin02@users.noreply.github.com>
2023-09-12 16:05:39 +0900

    [rm]불필요한 파일 정리

--- Cho-Hyunjin/StateAfter1/src/GameContext.java
+++
@@ -1,8 +0,0 @@
-// 게임 컨텍스트 인터페이스 정의
-public interface GameContext {
-    void setState(GameState state);
-    void startGame();
-    void pauseGame();
-    void resumeGame();
-    void gameOver();
-}
--- Cho-Hyunjin/StateAfter1/src/GameState.java
+++
@@ -1,8 +0,0 @@
-// 게임 상태 인터페이스 정의
-public interface GameState {
-    void start(GameContext context);
-    void pause(GameContext context);
-    void resume(GameContext context);
-    void gameOver(GameContext context);
-    String getDescription();
-}
