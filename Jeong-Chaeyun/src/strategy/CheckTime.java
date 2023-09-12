d3d70f227ca90fe73a4c84abe86a9dfc0ffeb6b6
kkkwp <chxrryda@gmail.com>
2023-09-12 23:57:55 +0900

    feat: strategy에 대한 javadocs

--- Jeong-Chaeyun/src/strategy/CheckTime.java
+++ Jeong-Chaeyun/src/strategy/CheckTime.java
@@ -3,8 +3,20 @@ package strategy;
 import java.time.Duration;
 import java.time.Instant;
 
+/** 이 클래스는 주어진 계산 전략을 받아와 피보나치 수열을 계산하고, 결과와 실행 시간을 출력하는 기능을 제공합니다.
+ *
+ * @see Strategy
+ * @see java.time.Duration
+ * @see java.time.Instant
+ */
 public class CheckTime {
 
+	/**
+	 * 주어진 계산 전략에 따라 피보나치 수열을 계산하고 실행 시간을 측정하여 출력합니다.
+	 *
+	 * @param n        계산할 피보나치 수열의 항 순서를 나타내는 정수입니다.
+	 * @param strategy 계산에 사용할 전략을 나타내는 {@link Strategy} 인터페이스 구현체입니다.
+	 */
 	public static void checkTime(int n, Strategy strategy) {
 
 		Instant beforeTime = Instant.now();
@@ -16,6 +28,14 @@ public class CheckTime {
 		print(name, n, result, diffTime);
 	}
 
+	/**
+	 * 결과와 실행 시간을 출력합니다.
+	 *
+	 * @param name     실행 전략의 클래스 이름입니다.
+	 * @param n        계산된 피보나치 수열의 항 순서를 나타내는 정수입니다.
+	 * @param result   계산된 피보나치 수열의 값입니다.
+	 * @param diffTime 실행 시간 (밀리초)입니다.
+	 */
 	private static void print(String name, int n, long result, long diffTime) {
 		System.out.println("======================================");
 		System.out.println(name + "의 결과: " + result);
--- Jeong-Chaeyun/src/strategy/MemoizationArrayStrategy.java
+++ Jeong-Chaeyun/src/strategy/MemoizationArrayStrategy.java
@@ -1,13 +1,33 @@
 package strategy;
 
+/**
+ * 배열을 사용한 메모이제이션을 활용하여 피보나치 수열을 계산하는 계산 전략 클래스입니다.
+ * 이 클래스는 {@link Strategy} 인터페이스를 구현하며, 배열을 활용하여 중복 계산을 피하고 피보나치 수열을 빠르게 계산합니다.
+ *
+ *  @see Strategy
+ */
 public class MemoizationArrayStrategy implements Strategy {
 
 	private static long[] array;
 
+	/**
+	 * MemoizationArrayStrategy 클래스의 생성자입니다.
+	 * 이 생성자는 피보나치 수열 계산을 위한 배열을 초기화합니다.
+	 * 배열의 크기는 입력 값 n에 1을 더한 크기로 설정됩니다.
+	 *
+	 * @param n 계산에 사용되는 입력 값으로, 피보나치 수열의 항 순서를 나타냅니다.
+	 */
 	public MemoizationArrayStrategy(int n) {
 		array = new long[n + 1];
 	}
 
+	/**
+	 * 주어진 순서에 해당하는 피보나치 수열의 값을 계산합니다.
+	 * 이 계산은 메모이제이션 배열을 활용하여 중복 계산을 피하고, 이미 계산된 결과를 가져와 사용합니다.
+	 *
+	 * @param n 계산할 피보나치 수열의 항 순서를 나타냅니다.
+	 * @return 주어진 순서에 해당하는 피보나치 수열의 값
+	 */
 	@Override
 	public long fibonacci(int n) {
 		if (n == 0)
