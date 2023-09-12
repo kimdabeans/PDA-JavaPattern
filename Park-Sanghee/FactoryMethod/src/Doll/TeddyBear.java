d4c03149c43f510203a8ef7909851da5106667ed
C0012S <alwaysh1002@gmail.com>
2023-09-12 18:22:19 +0900

    [COMMENT] TeddyBear

--- Park-Sanghee/FactoryMethod/src/Doll/TeddyBear.java
+++ Park-Sanghee/FactoryMethod/src/Doll/TeddyBear.java
@@ -2,10 +2,27 @@ package Doll;
 
 import Factory.Doll;
 
+/**
+ * ConcreteProduct : 곰인형을 나타내는 클래스
+ * @version 1.0
+ * @author Park SangHee
+ **/
 public class TeddyBear extends Doll {
+	/**
+	 * 동물의 종류를 나타내는 String 인스턴스
+	 **/
 	private String animal;
+	
+	/**
+	 * 인형의 이름을 나타내는 String 인스턴스
+	 **/
 	private String dollName;
 	
+	/**
+	 * 동물의 종류를 나타내는 문자열과 인형의 이름을 나타내는 문자열을 파라미터로 받는 TeddyBear 생성자
+	 * @param animal
+	 * @param dollName
+	 **/
 	TeddyBear(String animal, String dollName) {
 		System.out.println(animal + "인형을 만듭니다.");
 		this.animal = animal;
