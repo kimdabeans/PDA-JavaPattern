4c965023480014a3ad102c9c1fea80db3fddb3f4
C0012S <alwaysh1002@gmail.com>
2023-09-12 18:15:09 +0900

    [COMMENT] LRUStrategy

--- Park-Sanghee/Strategy/src/PageReplacement/LRUStrategy.java
+++ Park-Sanghee/Strategy/src/PageReplacement/LRUStrategy.java
@@ -1,5 +1,10 @@
 package PageReplacement;
 
+/**
+ * ConcreteStrategy : LRU 페이지 교체 알고리즘을 구현하는 클래스
+ * @version 1.0
+ * @author Park SangHee
+ **/
 public class LRUStrategy implements PageReplacementStrategy {
 	@Override
 	public void replace(int demandPage) {
