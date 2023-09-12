719fcab6511ea67165f3b53e8569270df2129758
eunsiver <76419984+eunsiver@users.noreply.github.com>
2023-09-12 18:53:25 +0900

    Update TradingStrategy.java

--- Lee-Eunji/src/strategy_pattern/example1/after/TradingStrategy.java
+++ Lee-Eunji/src/strategy_pattern/example1/after/TradingStrategy.java
@@ -1,7 +1,7 @@
 package strategy_pattern.example1.after;
 
 /**
- * 이 인터페이스는 다양한 매매 전략을 구현하는 데 사용됩니다.
+ * 해당 인터페이스는 다양한 매매 전략을 구현하는 데 사용됩니다.
  * 구체적인 전략 클래스는 executeStrategy() 메소드를 구현해야 합니다.
  */
 public interface TradingStrategy {
