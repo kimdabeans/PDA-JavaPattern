2d9a4d35bff4dbdc2d53ecd64d4b3f8d80e93e4a
eunsiver <76419984+eunsiver@users.noreply.github.com>
2023-09-12 18:54:04 +0900

    Update StockTrader.java

--- Lee-Eunji/src/strategy_pattern/example2/before/StockTrader.java
+++ Lee-Eunji/src/strategy_pattern/example2/before/StockTrader.java
@@ -4,7 +4,6 @@ package strategy_pattern.example2.before;
  * 추상 클래스 StockTrader는 주식 거래자의 기본 틀을 제공합니다.
  * 실제 주식 거래자 클래스는 이 클래스를 상속받아 구체적인 거래 전략을 구현해야 합니다.
  */
-
 public abstract class StockTrader {
     /**
      * 주식을 구매하는 추상 메서드입니다.
