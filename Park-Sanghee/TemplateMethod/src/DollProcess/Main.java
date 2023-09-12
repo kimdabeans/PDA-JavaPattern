c647cd4b0cf284ffd73b5d75efe432ad44abbdd3
C0012S <alwaysh1002@gmail.com>
2023-09-12 18:20:22 +0900

    [COMMENT] DollProcess Main

--- Park-Sanghee/TemplateMethod/src/DollProcess/Main.java
+++ Park-Sanghee/TemplateMethod/src/DollProcess/Main.java
@@ -1,12 +1,26 @@
 package DollProcess;
 
+/**
+ * Main Class : 인형을 만드는 과정을 수행하는 클래스
+ * @version 1.0
+ * @author Park SangHee
+ **/
 public class Main {
+	/**
+	 * 프로그램의 주요 실행 메서드
+	 * 토끼 인형과 고양이 인형을 만드는 과정 수행
+	 * @param args 명령행 인수 배열
+	 **/
 	public static void main(String[] args) {
 		DollProcess rabbitDoll = new RabbitDollProcess("Rabbit");
 		DollProcess catDoll = new CatDollProcess("Cat");
 		
+		// 토끼 인형 만들기
 		rabbitDoll.createDoll();
+		
 		System.out.println();
+		
+		// 고양이 인형 만들기
 		catDoll.createDoll();
 	}
 }
