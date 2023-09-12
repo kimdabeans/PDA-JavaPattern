916037d5310bc3f305d77c450bff336fe01418f7
C0012S <alwaysh1002@gmail.com>
2023-09-12 18:19:52 +0900

    [COMMENT] CatDollProcess

--- Park-Sanghee/TemplateMethod/src/DollProcess/CatDollProcess.java
+++ Park-Sanghee/TemplateMethod/src/DollProcess/CatDollProcess.java
@@ -1,8 +1,20 @@
 package DollProcess;
 
+/**
+ * ConcreteClass : 고양이 인형을 만드는 과정을 구현한 클래스
+ * @version 1.0
+ * @author Park SangHee
+ **/
 public class CatDollProcess extends DollProcess {
+	/**
+	 * 동물의 종류를 나타내는 String 인스턴스
+	 **/
 	private String animal;
 	
+	/**
+	 * 동물의 종류를 나타내는 문자열을 파라미터로 받는 CatDollProcess 생성자
+	 * @param animal 동물의 종류를 나타내는 문자열
+	 **/
 	public CatDollProcess(String animal) {
 		this.animal = animal;
 	}
@@ -29,7 +41,11 @@ public class CatDollProcess extends DollProcess {
 		System.out.println(animal + " 인형 다리를 조립합니다.");
 		System.out.println(animal + " 인형 발에 하트를 붙입니다.");
 	}
-	
+
+	/**
+	 * 고양이 귀를 조립하는 메서드
+	 * 고양이 귀를 2 개 붙인다.
+	 **/
 	private void assembleEars() {
 		for (int e = 0; e < 2; e++) {
 			System.out.println("☆ 고양이 귀를 붙입니다. ☆");
