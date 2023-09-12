2607fc3d0a4371717bc63e392a08c0811ebaa1c1
Jieun714 <jieun98.dev@gmail.com>
2023-09-12 17:16:09 +0900

    [DELETE] doc folder

--- Lee-Jieun/Before_Adapter/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"messageService","l":"EmailService"},{"p":"messageService","l":"EmailServiceAdapter"},{"p":"<Unnamed>","l":"Main"},{"p":"messageService","l":"MessageService"},{"p":"messageService","l":"SMSService"},{"p":"messageService","l":"SMSServiceAdapter"}];updateSearchResults();
